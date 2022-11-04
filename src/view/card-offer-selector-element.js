bddf8f66595720071a39a62580206f2abc3f14cb
max.b <max.b@dev.izibook.ru>
2022-11-04 23:47:07 +0300

    add component data

--- src/view/card-offer-selector-element.js
+++ src/view/card-offer-selector-element.js
@@ -1,10 +1,11 @@
-export const createCardOfferSelectorElementTemplate = () => (
-  `<div class="event__offer-selector">
-    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-    <label class="event__offer-label" for="event-offer-luggage-1">
-      <span class="event__offer-title">Add luggage</span>
+export const createCardOfferSelectorElementTemplate = (dataSelect) => {
+  const {title, price, name} = dataSelect;
+  return `<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-${name}-1" type="checkbox" name="event-offer-${name}" checked>
+    <label class="event__offer-label" for="event-offer-${name}-1">
+      <span class="event__offer-title">${title}</span>
       &plus;&euro;&nbsp;
-      <span class="event__offer-price">50</span>
+      <span class="event__offer-price">${price}</span>
     </label>
-  </div>`
-);
+  </div>`;
+};
